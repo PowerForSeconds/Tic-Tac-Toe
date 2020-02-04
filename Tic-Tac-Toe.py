00f8f0fba85cb7b6204cc695a304260b35dc9f75
PowerForSeconds <powerforseconds@gmail.com>
2020-02-04 11:05:14 -0800

    Players can now take turns placing pieces

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -9,3 +9,69 @@ def clearscreen():
             os.system("clear")
         except:
             pass
+
+board = [[" ", " ", " "],
+         [" ", " ", " "],
+         [" ", " ", " "]]
+
+turn = "X"
+
+while True:
+    clearscreen()
+    input("Player 1, press ENTER")
+    clearscreen()
+    while turn == "X":
+        answer = False
+        while answer == False:
+            clearscreen()
+            print("   1 2 3")
+            print("  +-+-+-+")
+            print("1 |" + board[0][0] + "|" + board[0][1] +"|" + board[0][2] + "|")
+            print("  +-+-+-+")
+            print("2 |" + board[1][0] + "|" + board[1][1] +"|" + board[1][2] + "|")
+            print("  +-+-+-+")
+            print("3 |" + board[2][0] + "|" + board[2][1] +"|" + board[2][2] + "|")
+            print("  +-+-+-+")
+            answer = input("Enter your choice now  ")
+            if len(answer) == 2:
+                try:
+                    answer = [int(answer[0]) - 1, int(answer[1]) - 1]
+                    if board[answer[1]][answer[0]] == " ":
+                        board[answer[1]][answer[0]] = "X"
+                    else:
+                        answer = False
+                except:
+                    answer = False
+            else:
+                answer = False
+            
+        turn = "O"
+    clearscreen()
+    input("Player 2, press ENTER")
+    clearscreen()
+    while turn == "O":
+        answer = False
+        while answer == False:
+            clearscreen()
+            print("   1 2 3")
+            print("  +-+-+-+")
+            print("1 |" + board[0][0] + "|" + board[0][1] +"|" + board[0][2] + "|")
+            print("  +-+-+-+")
+            print("2 |" + board[1][0] + "|" + board[1][1] +"|" + board[1][2] + "|")
+            print("  +-+-+-+")
+            print("3 |" + board[2][0] + "|" + board[2][1] +"|" + board[2][2] + "|")
+            print("  +-+-+-+")
+            answer = input("Enter your choice now  ")
+            if len(answer) == 2:
+                try:
+                    answer = [int(answer[0]) - 1, int(answer[1]) - 1]
+                    if board[answer[1]][answer[0]] == " ":
+                        board[answer[1]][answer[0]] = "O"
+                    else:
+                        answer = False
+                except:
+                    answer = False
+            else:
+                answer = False
+                
+        turn = "X"
